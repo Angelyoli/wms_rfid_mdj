999c182bb504b83780269271fd68a93e5bbf263d
loou <loou@vip.qq.com>
2012-09-04 12:26:31 +0800

    添加日结下载和从分拣线下载线路和修改线路调度 Signed-off-by: loou <loou@vip.qq.com>

--- code/Authority/THOK.Wms.DownloadWms/Dao/DownSortingInfoDao.cs
+++ code/Authority/THOK.Wms.DownloadWms/Dao/DownSortingInfoDao.cs
@@ -11,13 +11,17 @@ namespace THOK.Wms.DownloadWms.Dao
     {
         public DataTable GetSortingOrder(string parameter)
         {
-            string sql = string.Format("SELECT * FROM SORTORDER WHERE {0}", parameter);
+            string sql = string.Format(@"SELECT S.*,C.COMPANY_CODE,C.SALE_REGION_CODE,C.SALE_SCOPE,C.DELIVER_ORDER FROM WMS_SORT_ORDER S
+                                            LEFT JOIN WMS_CUSTOMER C ON S.CUSTOMER_CODE=C.CUSTOMER_CODE WHERE {0}", parameter);
             return this.ExecuteQuery(sql).Tables[0];
         }
 
         public DataTable GetSortingDetail(string parameter)
         {
-            string sql = string.Format("SELECT * FROM SORTORDERDETAIL WHERE {0}", parameter);
+            string sql = string.Format(@"SELECT S.*,P.TRADE_PRICE,(P.TRADE_PRICE*REALQUANTITY) AS AMOUNT_PRICE,
+                                                    U.UNIT_CODE02,U.QUANTITY01 FROM WMS_SORT_ORDER_DETAIL S
+                                                    LEFT JOIN WMS_PRODUCT P ON S.PRODUCT_CODE=P.PRODUCT_CODE
+                                                    LEFT JOIN WMS_UNIT_LIST U ON P.UNIT_LIST_CODE=U.UNIT_LIST_CODE WHERE {0}", parameter);
             return this.ExecuteQuery(sql).Tables[0];
         }
         
@@ -29,6 +33,11 @@ namespace THOK.Wms.DownloadWms.Dao
         public void InsertSortingOrderDetail(DataSet ds)
         {
             BatchInsert(ds.Tables["WMS_SORT_ORDER_DETAIL"], "WMS_SORT_ORDER_DETAIL");
+        }
+
+        public void InsertDispatch(DataSet ds)
+        {
+            BatchInsert(ds.Tables["WMS_SORT_ORDER_DISPATCH"], "WMS_SORT_ORDER_DISPATCH");
         }
 
         public DataTable GetOrderId()
