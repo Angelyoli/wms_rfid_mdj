7611705c2701b55812577513670ce17c5b133beb
NameJun <122419795@qq.com>
2012-09-11 15:26:19 +0800

    删除综合数据查询中入出移盘损方法 Signed-off-by: NameJun <122419795@qq.com>

--- code/Authority/THOK.Wms.Bll/Service/MoveSearchDetailService.cs
+++ code/Authority/THOK.Wms.Bll/Service/MoveSearchDetailService.cs
@@ -45,49 +45,5 @@ namespace THOK.Wms.Bll.Service
             return new { total, rows = moveDetail.ToArray() };
         }
         #endregion
-
-        public System.Data.DataTable GetMoveDetail(int page, int rows, string BillNo)
-        {
-            IQueryable<MoveBillDetail> moveBillDetailQuery = MoveSearchDetailRepository.GetQueryable();
-            var moveBillDetails = moveBillDetailQuery.Where(i => i.BillNo.Contains(BillNo)).OrderBy(i => i.BillNo).Select(i => new
-            {
-                i.ID,
-                i.BillNo,
-                i.ProductCode,
-                i.Product.ProductName,
-                i.UnitCode,
-                i.Unit.UnitName,
-                i.InCellCode,
-                PlaceName_In = i.InCell.CellName,
-                i.OutCellCode,
-                PlaceName_Out = i.OutCell.CellName,
-                RealQuantity = i.RealQuantity / i.Unit.Count,
-                i.Status
-            });
-            System.Data.DataTable dt = new System.Data.DataTable();
-            dt.Columns.Add("序号", typeof(string));
-            dt.Columns.Add("货位名称(出)", typeof(string));
-            dt.Columns.Add("货位名称(入)", typeof(string));
-            dt.Columns.Add("产品代码", typeof(string));
-            dt.Columns.Add("产品名称", typeof(string));
-            dt.Columns.Add("单位编码", typeof(string));
-            dt.Columns.Add("单位名称", typeof(string));
-            dt.Columns.Add("数量", typeof(int));
-            foreach (var m in moveBillDetails)
-            {
-                dt.Rows.Add
-                    (
-                        m.ID,
-                        m.PlaceName_Out,
-                        m.PlaceName_In,
-                        m.ProductCode,
-                        m.ProductName,
-                        m.UnitCode,
-                        m.UnitName,
-                        m.RealQuantity
-                    );
-            }
-            return dt;
-        }
     }
 }
