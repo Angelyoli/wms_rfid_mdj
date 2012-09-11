4ca8a87124025213a3580eb8df1fabfafed759b1
zqloon <zqloon@gmail.com>
2012-09-11 17:31:08 +0800

    修改了预警部分功能 Signed-off-by: zqloon <zqloon@gmail.com>

--- code/Authority/THOK.Wms.Bll/Service/ProductWarningService.cs
+++ code/Authority/THOK.Wms.Bll/Service/ProductWarningService.cs
@@ -26,28 +26,42 @@ namespace THOK.Wms.Bll.Service
          {
              get { return this.GetType(); }
          }
-         #region  卷烟预警设置的增，删，改，查方法
+        #region  卷烟预警设置的增，删，改，查方法
          public object GetDetail(int page, int rows, string productCode, decimal minLimited, decimal maxLimited, decimal assemblyTime)
          {
              IQueryable<ProductWarning> productWarningQuery = ProductWarningRepository.GetQueryable();
-             var productName = ProductRepository.GetQueryable().FirstOrDefault(p=>p.ProductCode==productCode);
-             var productWarning = productWarningQuery.Where(p => p.ProductCode.Contains( productCode)
-                 && p.MinLimited == minLimited && p.MaxLimited == maxLimited && p.AssemblyTime == assemblyTime)
-                 .OrderBy(p => p.ProductCode)
-                 .Select(p => new 
+             var productWarning = productWarningQuery.Where(p => p.ProductCode.Contains(productCode)).OrderBy(p => p.ProductCode).Select(p => p).ToArray();
+             if (productCode != "")
+             {
+                 productWarning = productWarning.Where(p => p.ProductCode == productCode).ToArray();
+             }
+             if (minLimited != 100000)
+             {
+                 productWarning = productWarning.Where(p => p.MinLimited <= minLimited).ToArray();
+             }
+             if (maxLimited != 100000)
+             {
+                 productWarning = productWarning.Where(p => p.MaxLimited >= maxLimited).ToArray();
+             }
+             if (assemblyTime != 3600)
+             {
+                 productWarning = productWarning.Where(p => p.AssemblyTime >= assemblyTime).ToArray();
+             }
+             var productWarn = productWarning
+                 .Select(p => new
                  {
                      p.ProductCode,
-                     ProductName=productName.ProductName,
+                     ProductName = ProductRepository.GetQueryable().FirstOrDefault(q => q.ProductCode==p.ProductCode).ProductName,
                      p.UnitCode,
                      p.Unit.UnitName,
-                     MinLimited=p.MinLimited/p.Unit.Count,
-                     MaxLimited=p.MaxLimited/p.Unit.Count,
+                     MinLimited = p.MinLimited / p.Unit.Count,
+                     MaxLimited = p.MaxLimited / p.Unit.Count,
                      p.AssemblyTime,
                      p.Memo
                  });
-             int total = productWarning.Count();
-             productWarning = productWarning.Skip((page - 1) * rows).Take(rows);
-             return new { total, rows = productWarning.ToArray() };
+             int total = productWarn.Count();
+             productWarn = productWarn.Skip((page - 1) * rows).Take(rows);
+             return new { total, rows = productWarn.ToArray() };
          }
 
          public bool Add(ProductWarning productWarning)
@@ -103,25 +117,38 @@ namespace THOK.Wms.Bll.Service
         #region 产品短缺、超储查询
          public object GetQtyLimitsDetail(int page, int rows, string productCode, decimal minLimited, decimal maxLimited, string unitCode)
          {
-             IQueryable<Storage> Quantity = StorageRepository.GetQueryable();
+             IQueryable<Storage> StorageQuery = StorageRepository.GetQueryable();
+             IQueryable<ProductWarning> ProductWarningQuery = ProductWarningRepository.GetQueryable();
              var unit = UnitRepository.GetQueryable().FirstOrDefault(u => u.UnitCode == unitCode);
-             var Qty = Quantity.Where(t => t.ProductCode.Contains(productCode))
-                 .GroupBy(t=>t.ProductCode)
-                 .Select(t => new 
+             var ProductWarning = ProductWarningQuery.Where(p => p.ProductCode.Contains(productCode)); 
+             var storageQuery = StorageQuery.Where(q => q.ProductCode.Contains(productCode))
+                                 .OrderBy(p => p.ProductCode).GroupBy(p=>p.ProductCode).Select(p=>p);
+             if(productCode!="")
+             {
+                 ProductWarning = ProductWarning.Where(p=>p.ProductCode == productCode);
+             }
+              if (minLimited != 100000)
+             {
+                 ProductWarning = ProductWarning.Where(p=>p.MinLimited<=minLimited * unit.Count);
+             }
+             if (maxLimited != 100000)
+             {
+                 ProductWarning = ProductWarning.Where(p => p.MaxLimited >= maxLimited * unit.Count);
+             }
+             var priductWarning = ProductWarning.Select(t => new 
                  {
-                     ProductCode =t.Max(p=>p.Product.ProductCode),
-                     ProductName=t.Max(p=>p.Product.ProductName),
-                     //UnitCode=t.Max(p=>p.ProductWarning.UnitCode),
-                     //UnitName = t.Max(p => p.ProductWarning.Unit.UnitName),
-                     Quantity = t.Sum(p => p.Quantity/unit.Count),
-                     //MinLimited = t.Max(p => p.ProductWarning.MinLimited / unit.Count),
-                     //MaxLimited = t.Max(p => p.ProductWarning.MaxLimited / unit.Count)
+                     ProductCode =t.ProductCode,
+                     ProductName = ProductRepository.GetQueryable().FirstOrDefault(q => q.ProductCode == t.ProductCode).ProductName,
+                     UnitCode=t.UnitCode,
+                     UnitName = t.Unit.UnitName,
+                     Quantity = StorageRepository.GetQueryable().Where(s=>s.ProductCode==t.ProductCode).Sum(s=>s.Quantity),
+                     MinLimited = minLimited/t.Unit.Count,
+                     MaxLimited = maxLimited / t.Unit.Count
                  });
-             var productWarn = Qty.Where(q => q.Quantity <= minLimited && q.Quantity >= maxLimited);
-             int total = productWarn.Count();
-             productWarn = productWarn.OrderBy(q => q.ProductCode);
-             productWarn = productWarn.Skip((page - 1) * rows).Take(rows);
-             return new { total, rows = productWarn.ToArray() };
+             int total = priductWarning.Count();
+             priductWarning = priductWarning.OrderBy(q => q.ProductCode);
+             priductWarning = priductWarning.Skip((page - 1) * rows).Take(rows);
+             return new { total, rows = priductWarning.ToArray() };
          }
         #endregion
 
