b960038be638b59d0253f075af56a949fd0c1883
NameJun <122419795@qq.com>
2012-09-12 17:06:30 +0800

    先上传 Signed-off-by: NameJun <122419795@qq.com>

--- code/Authority/Wms/Controllers/Wms/Organization/CompanyController.cs
+++ code/Authority/Wms/Controllers/Wms/Organization/CompanyController.cs
@@ -104,18 +104,24 @@ namespace Authority.Controllers.Organization
             string headFont = "微软雅黑"; Int16 headSize = 20;
             string colHeadFont = "Arial"; Int16 colHeadSize = 10; Int16 colHeadWidth = 300;
             string exportDate = "导出时间：" + System.DateTime.Now.ToString("yyyy-MM-dd");
-            string filename = headText + DateTime.Now.ToString("yyMMdd-HHmm-ss");
+            this.GetResponse(headText);
 
+            System.IO.MemoryStream ms = THOK.Common.ExportExcel.ExportDT(dt, null, headText, null, headFont, headSize,
+                colHeadFont, colHeadSize, colHeadWidth, exportDate);
+            return new FileStreamResult(ms, "application/ms-excel");
+        }
+        #endregion
+
+        #region this.GetResponse
+        private void GetResponse(string headText)
+        {
+            string filename = headText + DateTime.Now.ToString("yyMMdd-HHmm-ss");
             Response.Clear();
             Response.BufferOutput = false;
             Response.ContentEncoding = System.Text.Encoding.GetEncoding("GB2312");
             Response.AddHeader("Content-Disposition", "attachment;filename=" + Uri.EscapeDataString(filename) + ".xls");
             Response.ContentType = "application/ms-excel";
-
-            System.IO.MemoryStream ms = THOK.Common.ExportExcel.ExportDT(dt, null, headText, null, headFont, headSize,
-                colHeadFont, colHeadSize, colHeadWidth, exportDate);
-            return new FileStreamResult(ms, "application/ms-excel");
-        } 
+        }        
         #endregion
     }
 }
