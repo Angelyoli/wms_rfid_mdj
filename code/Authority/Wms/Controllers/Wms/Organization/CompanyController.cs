52f8d81790be23b3cf0a0ee80e40db9a7a28e4f5
NameJun <122419795@qq.com>
2012-09-12 16:38:27 +0800

    组织结构管理打印 Signed-off-by: NameJun <122419795@qq.com>

--- code/Authority/Wms/Controllers/Wms/Organization/CompanyController.cs
+++ code/Authority/Wms/Controllers/Wms/Organization/CompanyController.cs
@@ -89,5 +89,33 @@ namespace Authority.Controllers.Organization
             var company = CompanyService.GetParentName(page, rows, queryString, value);
             return Json(company, "text", JsonRequestBehavior.AllowGet);
         }
+
+        #region /Company/CreateExcelToClient/
+        public FileStreamResult CreateExcelToClient()
+        {
+            int page = 0, rows = 0;
+            string companyCode = Request.QueryString["companyCode"];
+            string companyName = Request.QueryString["companyName"];
+            string companyType = Request.QueryString["companyType"];
+            string isActive = Request.QueryString["isActive"];
+            
+            System.Data.DataTable dt = CompanyService.GetCompany(page, rows, companyCode, companyName, companyType, isActive);
+            string headText = "公司信息";
+            string headFont = "微软雅黑"; Int16 headSize = 20;
+            string colHeadFont = "Arial"; Int16 colHeadSize = 10; Int16 colHeadWidth = 300;
+            string exportDate = "导出时间：" + System.DateTime.Now.ToString("yyyy-MM-dd");
+            string filename = headText + DateTime.Now.ToString("yyMMdd-HHmm-ss");
+
+            Response.Clear();
+            Response.BufferOutput = false;
+            Response.ContentEncoding = System.Text.Encoding.GetEncoding("GB2312");
+            Response.AddHeader("Content-Disposition", "attachment;filename=" + Uri.EscapeDataString(filename) + ".xls");
+            Response.ContentType = "application/ms-excel";
+
+            System.IO.MemoryStream ms = THOK.Common.ExportExcel.ExportDT(dt, null, headText, null, headFont, headSize,
+                colHeadFont, colHeadSize, colHeadWidth, exportDate);
+            return new FileStreamResult(ms, "application/ms-excel");
+        } 
+        #endregion
     }
 }
