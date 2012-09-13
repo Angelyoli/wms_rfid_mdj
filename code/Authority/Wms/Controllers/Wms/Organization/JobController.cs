7a5a902aef05127c2f8bd907c6875b170ac877a2
wu <981145751@qq.com>
2012-09-13 16:50:00 +0800

    修改岗位信息页面加载方式 Signed-off-by: wu <981145751@qq.com>

--- code/Authority/Wms/Controllers/Wms/Organization/JobController.cs
+++ code/Authority/Wms/Controllers/Wms/Organization/JobController.cs
@@ -29,6 +29,16 @@ namespace Authority.Controllers.Wms.Organization
             return View();
         }
 
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        public ActionResult SearchPage()
+        {
+            return View();
+        }
+
         //
         // GET: /Job/Details/
 
