e0d6d4ef27042bbfc609a910030bc033a9d34c40
NameJun <122419795@qq.com>
2012-09-20 17:33:28 +0800

    测试用模版导出Excel Signed-off-by: NameJun <122419795@qq.com>

--- code/Authority/THOK.Common/ExportExcel.cs
+++ code/Authority/THOK.Common/ExportExcel.cs
@@ -468,5 +468,77 @@ namespace THOK.Common
             return rowIndex;
         }
         #endregion
+
+
+        /// <summary>
+        /// 文件下载
+        /// </summary>
+        /// <param name="_Request"></param>
+        /// <param name="_Response"></param>
+        /// <param name="_fileName"></param>
+        /// <param name="_fullPath"></param>
+        /// <param name="_speed"></param>
+        /// <returns></returns>
+        public static bool ResponseFile(HttpRequest _Request, HttpResponse _Response, string _fileName, string _fullPath, long _speed)
+        {
+            try
+            {
+                FileStream myFile = new FileStream(_fullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                BinaryReader br = new BinaryReader(myFile);
+                try
+                {
+                    _Response.AddHeader("Accept-Ranges", "bytes");
+                    _Response.Buffer = false;
+                    long fileLength = myFile.Length;
+                    long startBytes = 0;
+                    double pack = 10240;
+                    //10K bytes                
+                    //int sleep = 200;   //每秒5次   即5*10K bytes每秒     
+                    int sleep = (int)Math.Floor(1000 * pack / _speed) + 1;
+                    if (_Request.Headers["Range"] != null)
+                    {
+                        _Response.StatusCode = 206;
+                        string[] range = _Request.Headers["Range"].Split(new char[] { '=', '-' });
+                        startBytes = Convert.ToInt64(range[1]);
+                    }
+                    _Response.AddHeader("Content-Length", (fileLength - startBytes).ToString());
+                    if (startBytes != 0)
+                    {
+                        _Response.AddHeader("Content-Range", string.Format(" bytes {0}-{1}/{2}", startBytes, fileLength - 1, fileLength));
+                    }
+                    _Response.AddHeader("Connection", "Keep-Alive");
+                    _Response.ContentType = "application/octet-stream";
+                    _Response.AddHeader("Content-Disposition", "attachment;filename=" + HttpUtility.UrlEncode(_fileName, System.Text.Encoding.UTF8));
+                    br.BaseStream.Seek(startBytes, SeekOrigin.Begin);
+                    int maxCount = (int)Math.Floor((fileLength - startBytes) / pack) + 1;
+                    for (int i = 0; i < maxCount; i++)
+                    {
+                        if (_Response.IsClientConnected)
+                        {
+                            _Response.BinaryWrite(br.ReadBytes(int.Parse(pack.ToString())));
+                            System.Threading.Thread.Sleep(sleep);
+                        }
+                        else
+                        {
+                            i = maxCount;
+                        }
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+                finally
+                {
+                    br.Close();
+                    myFile.Close();
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
