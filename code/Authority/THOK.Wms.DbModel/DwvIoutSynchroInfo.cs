276354cb6f428bc23558301e3ac0ddbf954db03d
zqloon <zqloon@gmail.com>
2012-09-27 17:28:45 +0800

    添加上报功能 Signed-off-by: zqloon <zqloon@gmail.com>

---
+++ code/Authority/THOK.Wms.DbModel/DwvIoutSynchroInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace THOK.Wms.DbModel
+{
+    public class DwvIoutSynchroInfo
+    {
+        public Guid ID { get; set; }
+        public string SyncTypeCode { get; set; }
+        public string SyncTypeName { get; set; }
+        public string Description { get; set; }
+        public string UpdateDate { get; set; }
+        public string IsImport { get; set; }
+        public string Remark { get; set; }
+    }
+}
---
+++ code/Authority/Wms/Controllers/Wms/Upload/UploadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using THOK.Wms.Upload;
+using THOK.WMS.Upload.Bll;
+using THOK.WebUtil;
+
+namespace Wms.Controllers.Wms.Upload
+{
+    public class UploadController : Controller
+    {
+        //
+        // GET: /Upload/
+
+        public ActionResult Index()
+        {
+            ViewBag.hasSearch = true;
+            ViewBag.hasUpload = true;
+            ViewBag.hasDelete = true;
+            ViewBag.hasPrint = true;
+            ViewBag.hasHelp = true;
+            return View();
+        }
+        public ActionResult UploadDetails(FormCollection collection)
+        {
+
+            string beginDate = collection["BeginDate"] ?? "";
+            string endDate = collection["EndDate"] ?? "";
+            UploadBll upload = new UploadBll();
+            var result = upload.QuerySynchroInfo(beginDate, endDate);
+            return Json(result, "text", JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Upload()
+        {
+            UploadDate upload = new UploadDate();
+            bool result = upload.UploadInfoData();
+            string msg = result ? "上传成功" : "上传失败";
+            return Json(JsonMessageHelper.getJsonMessage(result, msg), "text", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
