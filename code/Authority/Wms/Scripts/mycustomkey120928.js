aa9ade23152fec01312f25f8e381fd8b98b53ee1
name07 <122419795@qq.com>
2012-09-28 14:47:23 +0800

    添加：卷烟信息管理 快捷键。 Signed-off-by: name07 <122419795@qq.com>

--- code/Authority/Wms/Scripts/mycustomkey120928.js
+++ code/Authority/Wms/Scripts/mycustomkey120928.js
@@ -2,16 +2,13 @@ $(document).keydown(function (e) {
     e = e ? e : window.event;
     var keyCode = e.keyCode ? e.keyCode : e.which ? e.which : e.charCode;
     if (keyCode == 13) {
-        //                alert('searchKey----' + searchKey
-        //                            + '\n' + 'addKey-------' + addKey
-        //                            + '\n' + 'editKey------' + editKey
-        //                            + '\n' + 'deleteKey----' + deleteKey);
+        //alert('searchKey--' + searchKey + '\n' + 'addKey--' + addKey + '\n' + 'editKey--' + editKey + '\n' + 'deleteKey--' + deleteKey);
         if (searchKey == true) {
             searchKey = false;
             if (module == "organization") {
                 GetQueryParams();
             }
-            if (module == "warehouse") {
+            if (module == "warehouse" || module == "cigarette" || module == "cigaretteUnitList") {
                 select();
             }
             if (module == "warehouseProductSet") {
@@ -22,10 +19,7 @@ $(document).keydown(function (e) {
             addKey = false;
             editKey = false;
             deleteKey = false;
-            if (module == "organization") {
-                save();
-            }
-            if (module == "warehouse") {
+            if (module == "organization" || module == "warehouse" || module == "cigarette" || module == "cigaretteUnitList") {
                 save();
             }
             if (module == "warehouseProductSet") {
@@ -34,12 +28,13 @@ $(document).keydown(function (e) {
         }
     }
     if (keyCode == 27) {
+        //alert('searchKey--' + searchKey + '\n' + 'addKey--' + addKey + '\n' + 'editKey--' + editKey + '\n' + 'deleteKey--' + deleteKey);
         if (searchKey == true) {
             searchKey = false;
             if (module == "organization") {
                 searchDialog.dialog('close');
             }
-            if (module == "warehouse") {
+            if (module == "warehouse" || module == "cigarette" || module == "cigaretteUnitList") {
                 $('#dlg-search').dialog('close');
             }
             if (module == "warehouseProductSet") {
@@ -53,11 +48,11 @@ $(document).keydown(function (e) {
             if (module == "organization") {
                 addDialog.dialog('close');
             }
-            if (module == "warehouse") {
+            if (module == "warehouse" || module == "warehouseProductSet" || module == "cigarette") {
                 $('#dlg').dialog('close');
             }
-            if (module == "warehouseProductSet") {
-                $('#dlg').dialog('close');
+            if (module == "cigaretteUnitList") {
+                $('#dlg-add').dialog('close');
             }
         }
     }
