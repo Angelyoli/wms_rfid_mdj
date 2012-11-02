034149ba5df71d92c4c152f332287e1a023524b2
name07 <122419795@qq.com>
2012-11-02 09:19:45 +0800

    参数设置 Signed-off-by: name07 <122419795@qq.com>

--- code/Authority/THOK.Authority.Bll/Interfaces/ISystemParameterService.cs
+++ code/Authority/THOK.Authority.Bll/Interfaces/ISystemParameterService.cs
@@ -5,8 +5,8 @@ namespace THOK.Authority.Bll.Interfaces
     public interface ISystemParameterService : IService<SystemParameter>
     {
         object GetSystemParameter(int page, int rows, SystemParameter systemParameter);
-        bool SetSystemParameter(SystemParameter systemParameter, out string error);
-        bool AddSystemParameter(SystemParameter systemParameter, out string error);
+        bool SetSystemParameter(SystemParameter systemParameter, string userName, out string error);
+        bool AddSystemParameter(SystemParameter systemParameter, string userName, out string error);
         bool DelSystemParameter(int id, out string error);
     }
 }
