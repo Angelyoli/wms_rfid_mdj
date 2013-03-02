430bb1ca5243c39b92a3277a00f4495796a9d3c1
zqloon <zqloon@gmail.com>
2013-03-02 08:31:53 +0800

    上报所需字段的修改 Signed-off-by: zqloon <zqloon@gmail.com>

--- code/Authority/THOK.Wms.Upload/Dao/UploadDao.cs
+++ code/Authority/THOK.Wms.Upload/Dao/UploadDao.cs
@@ -219,7 +219,7 @@ namespace THOK.WMS.Upload.Dao
                     //sql = string.Format("update DWV_IORG_CUSTOMER SET CUST_CODE='{0}',CUST_N='{1}',CUST_NAME='{2}',ORG_CODE='{3}',SALE_REG_CODE='{4}',CUST_TYPE='{5}',RTL_CUST_TYPE_CODE='{6}'," +
                     //   "CUST_GEO_TYPE_CODE='{7}',DIST_ADDRESS='{8}',DIST_PHONE='{9}',LICENSE_CODE='{10}',PRINCIPAL_NAME='{11}',UPDATE_DATE='{12}',ISACTIVE='{13}',IS_IMPORT='{14}',N_CUST_CODE='{15}',SEFL_CUST_FLG='{16}',FUNC_CUST_FLG='{17}',BUSI_CIRC_TYPE='{18}',CHAI_FLG='{19}',ALL_DAY_FLG='{20}',BUSI_HOUR='{21}',INFO_TERM='{22}',HEAD_LOGO='{23}',DELI_TIME='{24}',DELIVER_WAY='{25}',PAY_TYPE='{26}' WHERE CUST_CODE='{0}'"
                     //   , row["customer_code"], row["custom_code"],
-                    //   row["customer_name"], row["company_code"], row["sale_region_code"], row["sale_scope"], row["industry_type"], row["city_or_countryside"], row["address"], row["phone"],
+                    //   row["customer_name"], row["company_code"], row["sale_region_code"], row["customer_type"], row["industry_type"], row["city_or_countryside"], row["address"], row["phone"],
                     //   row["license_code"], row["principal_name"], date, row["is_active"], "0", row["uniform_code"], 1, 0, 1, 2, 2, 1, 3, 3, 2, 1, 2);
                     //this.ExecuteNonQuery(sql);
                 }
@@ -227,7 +227,7 @@ namespace THOK.WMS.Upload.Dao
                 {
                     sql = string.Format("INSERT INTO DWV_IORG_CUSTOMER(CUST_CODE,CUST_N,CUST_NAME,ORG_CODE,SALE_REG_CODE,CUST_TYPE,RTL_CUST_TYPE_CODE," +
                        "CUST_GEO_TYPE_CODE,DIST_ADDRESS,DIST_PHONE,LICENSE_CODE,PRINCIPAL_NAME,UPDATE_DATE,ISACTIVE,IS_IMPORT,N_CUST_CODE,SEFL_CUST_FLG,FUNC_CUST_FLG,BUSI_CIRC_TYPE,CHAI_FLG,ALL_DAY_FLG,BUSI_HOUR,INFO_TERM,HEAD_LOGO,DELI_TIME,DELIVER_WAY,PAY_TYPE)VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}','{13}','{14}','{15}','{16}','{17}','{18}','{19}','{20}' ,'{21}','{22}','{23}','{24}','{25}','{26}')", row["customer_code"], row["custom_code"],
-                       row["customer_name"], row["company_code"], row["sale_region_code"], row["sale_scope"], row["industry_type"], row["city_or_countryside"], row["address"], row["phone"],
+                       row["customer_name"], row["company_code"], row["sale_region_code"], row["customer_type"], row["industry_type"], row["city_or_countryside"], row["address"], row["phone"],
                        row["license_code"], row["principal_name"], date, row["is_active"], "0", row["uniform_code"], 1, 0, 1, 2, 2, 1, 3, 3, 2, 1, 2);
                     this.ExecuteNonQuery(sql);
                 }
