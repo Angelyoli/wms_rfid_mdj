c0e93a2f8c65c2afe4e3fa3240a8d9dfc57db325
Endis <369029432@qq.com>
2013-03-21 11:50:19 +0800

    新增WCS各基础信息表结构 Signed-off-by: Endis <369029432@qq.com>

---
+++ code/Authority/THOK.Wms.Bll/Service/PositionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using THOK.Wms.Bll.Interfaces;
+using THOK.Wms.Dal.Interfaces;
+using THOK.Wms.DbModel;
+
+namespace THOK.Wms.Bll.Service
+{
+    public class PositionService : ServiceBase<Position>, IPositionService
+    {
+        [Dependency]
+        public IPositionRepository PositionRepository { get; set; }
+
+        protected override Type LogPrefix
+        {
+            get { return this.GetType(); }
+        }
+    }
+}
---
+++ code/Authority/THOK.Wms.Bll/Service/ProductSizeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using THOK.Wms.Bll.Interfaces;
+using THOK.Wms.Dal.Interfaces;
+using THOK.Wms.DbModel;
+
+namespace THOK.Wms.Bll.Service
+{
+    public class ProductSizeService : ServiceBase<ProductSize>, IProductSizeService
+    {
+        [Dependency]
+        public IProductSizeRepository ProductSizeRepository { get; set; }
+
+        protected override Type LogPrefix
+        {
+            get { return this.GetType(); }
+        }
+    }
+}
