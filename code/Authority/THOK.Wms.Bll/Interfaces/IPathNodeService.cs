43388296e6c5598c421b6e0528177f2ce19a975d
Dong-Yang <772369620@qq.com>
2013-03-27 14:09:03 +0800

    路径节点信息管理  未完成 Signed-off-by: Dong-Yang <772369620@qq.com>

--- code/Authority/THOK.Wms.Bll/Interfaces/IPathNodeService.cs
+++ code/Authority/THOK.Wms.Bll/Interfaces/IPathNodeService.cs
@@ -4,6 +4,16 @@ namespace THOK.Wms.Bll.Interfaces
 {
     public interface IPathNodeService : IService<PathNode>
     {
+    //    object GetDetails(int page, int rows, string ID, string PathID, string PositionID, string PathNodeOrder);
 
+        bool Add(PathNode PathNode, out string strResult);
+
+        bool Save(PathNode PathNode, out string strResult);
+
+        bool Delete(int PathID, out string strResult);
+
+        //object GetPathNode(int page, int rows, string queryString, string value);
+
+        //System.Data.DataTable GetPathNode(int page, int rows, string srmName, string state, string t);
     }
 }
