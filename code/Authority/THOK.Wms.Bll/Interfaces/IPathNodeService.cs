9baabcc86f6e9db6aec1168b64e96e58561d3dd6
Dong-Yang <772369620@qq.com>
2013-03-28 15:01:33 +0800

    PathNode Service Signed-off-by: Dong-Yang <772369620@qq.com>

--- code/Authority/THOK.Wms.Bll/Interfaces/IPathNodeService.cs
+++ code/Authority/THOK.Wms.Bll/Interfaces/IPathNodeService.cs
@@ -4,16 +4,25 @@ namespace THOK.Wms.Bll.Interfaces
 {
     public interface IPathNodeService : IService<PathNode>
     {
-    //    object GetDetails(int page, int rows, string ID, string PathID, string PositionID, string PathNodeOrder);
+        object GetDetails(int page, int rows, string ID, string PathID, string PositionID, string PathNodeOrder);
 
-        bool Add(PathNode PathNode, out string strResult);
 
-        bool Save(PathNode PathNode, out string strResult);
+        //bool Save(PathNode PathNode, out string strResult);
 
-        bool Delete(int PathID, out string strResult);
+        //bool Delete(int PathID, out string strResult);
+
+        //object GetPathNode(int page, int rows, string PathID);
+
+        //System.Data.DataTable GetPathNode(int page, int rows, string ID, string PathID, string PositionID, string PathNodeOrder);
+
+        ////bool Add(PathNode ID, out string strResult);
+
+        ////bool Save(PathNode ID, out string strResult);
 
         //object GetPathNode(int page, int rows, string queryString, string value);
 
-        //System.Data.DataTable GetPathNode(int page, int rows, string srmName, string state, string t);
+        bool Add(PathNode PathNode, string strResult);
+
+        bool Save(PathNode ID, string strResult);
     }
 }
--- code/Authority/THOK.Wms.Bll/Service/PathNodeService.cs
+++ code/Authority/THOK.Wms.Bll/Service/PathNodeService.cs
@@ -18,6 +18,99 @@ namespace THOK.Wms.Bll.Service
             get { return this.GetType(); }
         }
 
+        public object GetDetails(int page, int rows, string ID, string PathID, string PositionID, string PathNodeOrder)
+        {
+
+            string i = "";
+            IQueryable<PathNode> PathNodeQuery = PathNodeRepository.GetQueryable();
+            int Id = Convert.ToInt32(i);
+            var PathNode = PathNodeQuery.Where(p => p.ID == Id)
+                .OrderBy(p => p.ID).AsEnumerable().
+                Select(p => new
+                {
+                    p.ID,
+                    p.PathID,
+                    p.PositionID,
+                    p.PathNodeOrder,
+                });
+            int id = -1;
+            int sizeno = -1;
+            if ((ID != "" && ID != null) && (PositionID == "" || PositionID == null))
+            {
+                try
+                {
+                    id = Convert.ToInt32(ID);
+                }
+                catch
+                {
+                    id = -1;
+                }
+                finally
+                {
+                    var pn = PathNodeQuery.Where(p => p.ID == Id)
+                        .OrderBy(p => p.ID).AsEnumerable().
+                        Select(p => new
+                        {
+                            p.ID,
+                            p.PathID,
+                            p.PositionID,
+                            p.PathNodeOrder,
+                        });
+                }
+            }
+            else if ((ID == "" || ID == null) && (PositionID != "" && PositionID != null))
+            {
+                try
+                {
+                    sizeno = Convert.ToInt32(PositionID);
+                }
+                catch
+                {
+                    sizeno = -1;
+                }
+                finally
+                {
+                    var pn = PathNodeQuery.Where(p => p.ID == Id)
+                    .OrderBy(p => p.ID).AsEnumerable().
+                    Select(p => new
+                    {
+                        p.ID,
+                        p.PathID,
+                        p.PositionID,
+                        p.PathNodeOrder,
+                    });
+                }
+            }
+            else if ((ID != "" && ID != null) && (PositionID != "" && PositionID != null))
+            {
+                try
+                {
+                    id = Convert.ToInt32(ID);
+                    sizeno = Convert.ToInt32(PositionID);
+                }
+                catch
+                {
+                    id = -1;
+                }
+                finally
+                {
+                    var pn = PathNodeQuery.Where(p => p.ID == Id)
+                    .OrderBy(p => p.ID).AsEnumerable().
+                    Select(p => new
+                    {
+                        p.ID,
+                        p.PathID,
+                        p.PositionID,
+                        p.PathNodeOrder,
+                    });
+                }
+            }
+            int total = PathNode.Count();
+            PathNode = PathNode.Skip((page - 1) * rows).Take(rows);
+            return new { total, rows = PathNode.ToArray() };
+        }
+
+
 
         public bool Add(PathNode PathNode, out string strResult)
         {
@@ -28,7 +121,7 @@ namespace THOK.Wms.Bll.Service
             {
                 try
                 {
-                    pn.PathID = pn.PathID;
+                    //pn.PathID = pn.PathID;
                     pn.PositionID = PathNode.PositionID;
                     pn.PathNodeOrder = PathNode.PathNodeOrder;
 
@@ -76,103 +169,116 @@ namespace THOK.Wms.Bll.Service
             }
             return result;
         }
-        public bool Delete(int PathID, out string strResult)
+
+
+    //    public bool Delete(int PathID, out string strResult)
+    //    {
+    //        strResult = string.Empty;
+    //        bool result = false;
+    //        var PathNode = PathNodeRepository.GetQueryable().FirstOrDefault(p => p.ID == PathID);
+    //        if (PathNode != null)
+    //        {
+    //            try
+    //            {
+    //                PathNodeRepository.Delete(PathNode);
+    //                PathNodeRepository.SaveChanges();
+    //                result = true;
+    //            }
+    //            catch (Exception)
+    //            {
+    //                strResult = "原因：已在使用";
+    //            }
+    //        }
+    //        else
+    //        {
+    //            strResult = "原因：未找到当前需要删除的数据！";
+    //        }
+    //        return result;
+    //    }
+
+    //    //public object GetDetails(int page, int rows, string ID, string PathID, string PositionID, string PathNodeOrder)
+    //    //public object GetDetails(int page, int rows, string ID, string SRMName, string Description, string State)
+    //    public object GetPathNode(int page, int rows, string queryString, string value)
+    //    {
+    //        string id = "", PathID = "";
+
+    //        if (queryString == "id")
+    //        {
+    //            id = value;
+    //        }
+    //        else
+    //        {
+    //            PathID = value;
+    //        }
+    //        IQueryable<PathNode> PathNodeQuery = PathNodeRepository.GetQueryable();
+    //        int Id = Convert.ToInt32(id);
+    //        var PathNode = PathNodeQuery.Where(p => p.ID == Id )
+    //            .OrderBy(p => p.ID).AsEnumerable().
+    //            Select(p => new
+    //            {
+    //                p.ID,
+    //                p.PathID,
+    //                p.PositionID,
+    //                p.PathNodeOrder,
+    //            });
+    //        int total = PathNode.Count();
+    //        PathNode = PathNode.Skip((page - 1) * rows).Take(rows);
+    //        return new { total, rows = PathNode.ToArray() };
+    //    }
+
+    //    public System.Data.DataTable GetPathNode(int page, int rows, string PathID, string state, string t)
+    //    {
+    //        string id = "";
+    //        IQueryable<PathNode> PathNodeQuery = PathNodeRepository.GetQueryable();
+    //        int Id = Convert.ToInt32(id);
+    //        var PathNode = PathNodeQuery.Where(p => p.ID == Id)
+    //            .OrderBy(p => p.ID).AsEnumerable()
+    //            .Select(p => new
+    //            {
+    //                p.ID,
+    //                p.PathID,
+    //                p.PositionID,
+    //                p.PathNodeOrder,
+    //            });
+    //        if (!state.Equals(""))
+    //        {
+    //            PathNode = PathNodeQuery.Where(p => p.ID == Id)
+    //                .OrderBy(p => p.ID).AsEnumerable()
+    //                .Select(p => new
+    //                {
+    //                    p.ID,
+    //                    p.PathID,
+    //                    p.PositionID,
+    //                    p.PathNodeOrder,
+    //                });
+    //        }
+    //        System.Data.DataTable dt = new System.Data.DataTable();
+    //        dt.Columns.Add("ID", typeof(string));
+    //        dt.Columns.Add("路径ID", typeof(string));
+    //        dt.Columns.Add("位置ID", typeof(string));
+    //        dt.Columns.Add("路径节点顺序", typeof(string));
+    //        foreach (var item in PathNode)
+    //        {
+    //            dt.Rows.Add
+    //                (
+    //                    item.ID,
+    //                    item.PathID,
+    //                    item.PositionID,
+    //                    item.PathNodeOrder
+    //                );
+    //        }
+    //        return dt;
+    //    }
+
+        public bool Add(PathNode PathNode, string strResult)
         {
-            strResult = string.Empty;
-            bool result = false;
-            var PathNode = PathNodeRepository.GetQueryable().FirstOrDefault(p => p.ID == PathID);
-            if (PathNode != null)
-            {
-                try
-                {
-                    PathNodeRepository.Delete(PathNode);
-                    PathNodeRepository.SaveChanges();
-                    result = true;
-                }
-                catch (Exception)
-                {
-                    strResult = "原因：已在使用";
-                }
-            }
-            else
-            {
-                strResult = "原因：未找到当前需要删除的数据！";
-            }
-            return result;
+            throw new NotImplementedException();
         }
 
-        //public object GetDetails(int page, int rows, string ID, string PathID, string PositionID, string PathNodeOrder)
-        //public object GetDetails(int page, int rows, string ID, string SRMName, string Description, string State)
-        public object GetPathNode(int page, int rows, string queryString, string value)
-        {
-            string id = "", PathID = "";
 
-            if (queryString == "id")
-            {
-                id = value;
-            }
-            else
-            {
-                PathID = value;
-            }
-            IQueryable<PathNode> PathNodeQuery = PathNodeRepository.GetQueryable();
-            int Id = Convert.ToInt32(id);
-            var PathNode = PathNodeQuery.Where(p => p.ID == Id )
-                .OrderBy(p => p.ID).AsEnumerable().
-                Select(p => new
-                {
-                    p.ID,
-                    p.PathID,
-                    p.PositionID,
-                    p.PathNodeOrder,
-                });
-            int total = PathNode.Count();
-            PathNode = PathNode.Skip((page - 1) * rows).Take(rows);
-            return new { total, rows = PathNode.ToArray() };
-        }
-
-        public System.Data.DataTable GetPathNode(int page, int rows, string PathID, string state, string t)
+        public bool Save(PathNode ID, string strResult)
         {
-            string id = "";
-            IQueryable<PathNode> PathNodeQuery = PathNodeRepository.GetQueryable();
-            int Id = Convert.ToInt32(id);
-            var PathNode = PathNodeQuery.Where(p => p.ID == Id)
-                .OrderBy(p => p.ID).AsEnumerable()
-                .Select(p => new
-                {
-                    p.ID,
-                    p.PathID,
-                    p.PositionID,
-                    p.PathNodeOrder,
-                });
-            if (!state.Equals(""))
-            {
-                PathNode = PathNodeQuery.Where(p => p.ID == Id)
-                    .OrderBy(p => p.ID).AsEnumerable()
-                    .Select(p => new
-                    {
-                        p.ID,
-                        p.PathID,
-                        p.PositionID,
-                        p.PathNodeOrder,
-                    });
-            }
-            System.Data.DataTable dt = new System.Data.DataTable();
-            dt.Columns.Add("ID", typeof(string));
-            dt.Columns.Add("路径ID", typeof(string));
-            dt.Columns.Add("位置ID", typeof(string));
-            dt.Columns.Add("路径节点顺序", typeof(string));
-            foreach (var item in PathNode)
-            {
-                dt.Rows.Add
-                    (
-                        item.ID,
-                        item.PathID,
-                        item.PositionID,
-                        item.PathNodeOrder
-                    );
-            }
-            return dt;
+            throw new NotImplementedException();
         }
     }
 }
