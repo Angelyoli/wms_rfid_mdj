e9bbdcd630cddad6c6406b48fbd1863b83bb61c6
zqb <940658539@qq.com>
2013-03-28 11:09:08 +0800

    货位位置信息（未完成） Signed-off-by: zqb <940658539@qq.com>

--- code/Authority/THOK.Wms.Bll/Interfaces/ICellPositionService.cs
+++ code/Authority/THOK.Wms.Bll/Interfaces/ICellPositionService.cs
@@ -4,6 +4,12 @@ namespace THOK.Wms.Bll.Interfaces
 {
     public interface ICellPositionService : IService<CellPosition>
     {
+        object GetDetails(int page, int rows, string CellCode, string StockInPosition, string StockOutPosition);
 
+        bool Add(CellPosition cellPosition, out string strResult);
+
+        bool Save(CellPosition cellPosition, out string strResult);
+
+        bool Delete(int sizeId, out string strResult);
     }
 }
---
+++ code/Authority/Wms/Controllers/Wms/BasisInfo/CellPositionController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using THOK.Wms.Bll.Interfaces;
+using THOK.Wms.DbModel;
+using THOK.WebUtil;
+
+namespace Wms.Controllers.Wms.BasisInfo
+{
+    public class CellPositionController : Controller
+    {
+        [Dependency]
+        public ICellPositionService CellPositionService { get; set; }
+        //
+        // GET: /CellPosition/
+
+        public ActionResult Index(string moduleID)
+        {
+            ViewBag.hasSearch = true;
+            ViewBag.hasAdd = true;
+            ViewBag.hasEdit = true;
+            ViewBag.hasDelete = true;
+            ViewBag.hasPrint = true;
+            ViewBag.hasHelp = true;
+            ViewBag.ModuleID = moduleID;
+            return View();
+        }
+
+        //
+        // GET: /CellPosition/Details/
+        public ActionResult Details(int page, int rows, FormCollection collection)
+        {
+            string CellCode = collection["CellCode"] ?? "";
+            string StockInPosition = collection["StockInPosition"] ?? "";
+            string StockOutPosition = collection["StockOutPosition"] ?? "";
+            var productSize = CellPositionService.GetDetails(page, rows,CellCode, StockInPosition, StockOutPosition);
+            return Json(productSize, "text", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult SearchPage()
+        {
+            return View();
+        }
+
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        //
+        // POST: /CellPosition/Create/
+        [HttpPost]
+        public ActionResult Create(CellPosition cellPosition)
+        {
+            string strResult = string.Empty;
+            bool bResult = CellPositionService.Add(cellPosition, out strResult);
+            string msg = bResult ? "新增成功" : "新增失败";
+            return Json(JsonMessageHelper.getJsonMessage(bResult, msg, null), "text", JsonRequestBehavior.AllowGet);
+        }
+
+        //
+        // POST: /CellPosition/Edit/5
+        public ActionResult Edit(CellPosition cellPosition)
+        {
+            string strResult = string.Empty;
+            bool bResult = CellPositionService.Save(cellPosition, out strResult);
+            string msg = bResult ? "修改成功" : "修改失败";
+            return Json(JsonMessageHelper.getJsonMessage(bResult, msg, strResult), "text", JsonRequestBehavior.AllowGet);
+        }
+
+
+        //
+        // POST: /CellPosition/Delete/
+        [HttpPost]
+        public ActionResult Delete(int cellPositionId)
+        {
+            string strResult = string.Empty;
+            bool bResult = false;
+            bResult = CellPositionService.Delete(cellPositionId, out strResult);
+            string msg = bResult ? "删除成功" : "删除失败";
+            return Json(JsonMessageHelper.getJsonMessage(bResult, msg, strResult), "text", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
