c489ae9d238ebf7b2a662dd8348ba08c6bb4770a
yaojianfeng <759775741@qq.com>
2013-04-22 11:41:52 +0800

    货位位置信息修改
    Signed-off-by: yaojianfeng <759775741@qq.com>

--- code/Authority/Wms/Controllers/Wms/BasisInfo/CellPositionController.cs
+++ code/Authority/Wms/Controllers/Wms/BasisInfo/CellPositionController.cs
@@ -58,7 +58,7 @@ namespace Wms.Controllers.Wms.BasisInfo
         public ActionResult Create(CellPosition cellPosition)
         {
             string strResult = string.Empty;
-            bool bResult = CellPositionService.Add(cellPosition, out strResult);
+            bool bResult = CellPositionService.Add(cellPosition);
             string msg = bResult ? "新增成功" : "新增失败";
             return Json(JsonMessageHelper.getJsonMessage(bResult, msg, null), "text", JsonRequestBehavior.AllowGet);
         }
@@ -68,7 +68,7 @@ namespace Wms.Controllers.Wms.BasisInfo
         public ActionResult Edit(CellPosition cellPosition)
         {
             string strResult = string.Empty;
-            bool bResult = CellPositionService.Save(cellPosition, out strResult);
+            bool bResult = CellPositionService.Save(cellPosition);
             string msg = bResult ? "修改成功" : "修改失败";
             return Json(JsonMessageHelper.getJsonMessage(bResult, msg, strResult), "text", JsonRequestBehavior.AllowGet);
         }
@@ -81,7 +81,7 @@ namespace Wms.Controllers.Wms.BasisInfo
         {
             string strResult = string.Empty;
             bool bResult = false;
-            bResult = CellPositionService.Delete(cellPositionId, out strResult);
+            bResult = CellPositionService.Delete(cellPositionId);
             string msg = bResult ? "删除成功" : "删除失败";
             return Json(JsonMessageHelper.getJsonMessage(bResult, msg, strResult), "text", JsonRequestBehavior.AllowGet);
         }
