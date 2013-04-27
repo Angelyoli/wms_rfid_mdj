10757f96b3c1f6c7e1a7283a3e7cd06aef15442b
Dong-Yang <772369620@qq.com>
2013-04-27 09:38:06 +0800

    Signed-off-by: Dong-Yang <772369620@qq.com>

--- code/Authority/THOK.Wms.Bll/Interfaces/IPathService.cs
+++ code/Authority/THOK.Wms.Bll/Interfaces/IPathService.cs
@@ -8,16 +8,16 @@ namespace THOK.Wms.Bll.Interfaces
 {
     public interface IPathService : IService<Path>
     {
-        object GetDetails(int page, int rows, string OriginRegion, string PathName, string TargetRegion, string State);
+        object GetDetails(int page, int rows, Path path);
 
-        bool Add(Path path, out string strResult);
+        bool Add(Path path);
 
-        bool Save(Path path, out string strResult);
+        bool Save(Path path);
 
-        bool Delete(int pathId, out string strResult);
+        bool Delete(int pathId);
 
         object GetPath(int page, int rows, string queryString, string value);
 
-        System.Data.DataTable GetPath(int page, int rows, string Id, string pathName, string originId, string targetId, string state);
+        System.Data.DataTable GetPath(int page, int rows, Path path);
     }
 }
