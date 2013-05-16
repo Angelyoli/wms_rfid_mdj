0aa9871e1bb12b0a6144fbd460a57ec2a31faa8f
loou <loou@vip.qq.com>
2013-05-16 15:28:59 +0800

    wms入库和移库显示主表添加总数量 wes部分代码删除和修改 Signed-off-by: loou <loou@vip.qq.com>

--- code/Authority/THOK.Wms.Bll/Service/MoveBillMasterService.cs
+++ code/Authority/THOK.Wms.Bll/Service/MoveBillMasterService.cs
@@ -107,6 +107,7 @@ namespace THOK.Wms.Bll.Service
                 OperatePersonName = i.OperatePerson.EmployeeName,
                 VerifyPersonID = i.VerifyPersonID == null ? string.Empty : i.VerifyPerson.EmployeeCode,
                 VerifyPersonName = i.VerifyPersonID == null ? string.Empty : i.VerifyPerson.EmployeeName,
+                SumQuantity = i.MoveBillDetails.Sum(s => s.RealQuantity / s.Product.Unit.Count),
                 VerifyDate = (i.VerifyDate == null ? "" : ((DateTime)i.VerifyDate).ToString("yyyy-MM-dd HH:mm:ss")),
                 Status = WhatStatus(i.Status),
                 IsActive = i.IsActive == "1" ? "可用" : "不可用",
