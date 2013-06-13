b7b0456ccc1b7fd66838abca45cd34333aaf03ee
zengys <ynnuyonw@126.com>
2013-06-13 15:27:03 +0800

    添加，调整WCS、WMS项目目录；

---
+++ code/Authority/THOK.WCS.Bll/Interfaces/ITaskService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace THOK.WCS.Bll.Interfaces
+{
+    public interface ITaskService
+    {
+        bool CreateNewTaskFromInBill(string billNo);
+
+        bool CreateNewTaskFromOutBill(string billNo);
+
+        bool CreateNewTaskFromMoveBill(string billNo);
+
+        bool CreateNewTaskFromCheckBill(string billNo);
+
+        bool CreateNewTaskForEmptyPalletStack(int positionID);
+
+        bool CreateNewTaskForEmptyPalletSupply(int positionID);
+
+        bool CreateNewTaskForMoveBackRemain(int positionID);
+
+        bool FinishTask(int taskID);        
+    }
+}
