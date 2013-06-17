43dd92efa11afe446568700b7af2a7a4f3c3d163
yaojianfeng <759775741@qq.com>
2013-06-17 14:23:38 +0800

    货位位置页面，卷烟尺寸信息页面修改完成
    Signed-off-by: yaojianfeng <759775741@qq.com>

--- code/Authority/THOK.Wms.Bll/Service/CellPositionService.cs
+++ code/Authority/THOK.Wms.Bll/Service/CellPositionService.cs
@@ -60,42 +60,11 @@ namespace THOK.Wms.Bll.Service
             return new { total, rows = cellPosition.ToArray() };
         }
 
-
-        //public object GetDetails(int page, int rows,string CellCode, string StockInPosition, string StockOutPosition)
-        //{
-        //    IQueryable<CellPosition> cellPositionQuery = CellPositionRepository.GetQueryable();
-        //    IQueryable<Position> positionQuery = PositionRepository.GetQueryable();
-        //    IQueryable<Cell> cellQuery = CellRepository.GetQueryable();
-
-        //    var cellPosition = cellPositionQuery.Join(positionQuery,
-        //                                 c => c.StockInPositionID,
-        //                                 p1 => p1.ID,
-        //                                 (c, p1) => new { c.ID, c.CellCode, c.StockInPositionID, c.StockOutPositionID, InName= p1.PositionName })
-        //                                 .Join(positionQuery,
-        //                                 c => c.StockOutPositionID,
-        //                                 p2 => p2.ID,
-        //                                 (c, p2) => new { c.ID, c.CellCode, c.StockInPositionID, c.StockOutPositionID, c.InName, OutName=p2.PositionName })
-        //                                 .Where(p=>p.CellCode.Contains(CellCode) && p.InName.Contains(StockInPosition)&&p.OutName.Contains(StockOutPosition))
-        //                                 .OrderBy(p => p.ID).AsEnumerable()
-        //                                 .Select(p => new
-        //                                 {
-        //                                     p.ID,
-        //                                     p.CellCode,
-        //                                     p.StockInPositionID,
-        //                                     p.InName,
-        //                                     p.StockOutPositionID,
-        //                                     p.OutName
-        //                                 });
-            
-        //    int total = cellPosition.Count();
-        //    cellPosition = cellPosition.Skip((page - 1) * rows).Take(rows);
-        //    return new { total, rows = cellPosition.ToArray() };
-        //}
-
         public bool Add(CellPosition cellPosition)
         {
             var cp = new CellPosition();
            // cp.ID = cellPosition.ID;
+            //cp.CellCode = cellPosition.CellCode;
             cp.CellCode = cellPosition.CellCode;
             cp.StockInPositionID = cellPosition.StockInPositionID;
             cp.StockOutPositionID = cellPosition.StockOutPositionID;
@@ -109,11 +78,12 @@ namespace THOK.Wms.Bll.Service
             strResult = string.Empty;
             bool result = false;
             var c = CellPositionRepository.GetQueryable().FirstOrDefault(s => s.ID == CellPosition.ID);
-
+           
             if (c != null)
             {
                 try
                 {
+                    c.ID = CellPosition.ID;
                     c.CellCode = CellPosition.CellCode;
                     c.StockInPositionID = CellPosition.StockInPositionID;
                     c.StockOutPositionID = CellPosition.StockOutPositionID;
@@ -152,6 +122,7 @@ namespace THOK.Wms.Bll.Service
         {
             IQueryable<CellPosition> cellPositionQuery = CellPositionRepository.GetQueryable();
             IQueryable<Position> positionQuery = PositionRepository.GetQueryable();
+            IQueryable<Cell> cellQuery = CellRepository.GetQueryable();
 
             var cellPosition = cellPositionQuery.Join(positionQuery,
                                          c => c.StockInPositionID,
@@ -161,6 +132,11 @@ namespace THOK.Wms.Bll.Service
                                          c => c.StockOutPositionID,
                                          p2 => p2.ID,
                                          (c, p2) => new { c.ID, c.CellCode, c.StockInPositionID, c.StockOutPositionID, c.InName, OutName = p2.PositionName })
+                                          .Join(cellQuery,
+                                         c => c.CellCode,
+                                         cq => cq.CellCode,
+                                         (c, cq) => new { c.ID, c.CellCode, c.StockInPositionID, c.StockOutPositionID, CellName = cq.CellName, c.InName, c.OutName }
+                                         )
                                          .Where(p => p.CellCode.Contains(cp.CellCode))
                                          .OrderBy(p => p.ID).AsEnumerable()
                                          .Select(p => new
@@ -168,11 +144,13 @@ namespace THOK.Wms.Bll.Service
                                              p.ID,
                                              p.CellCode,
                                              p.InName,
-                                             p.OutName
+                                             p.OutName,
+                                             p.CellName
                                          });
             System.Data.DataTable dt = new System.Data.DataTable();
             dt.Columns.Add("货位ID", typeof(string));
             dt.Columns.Add("货位代码", typeof(string));
+            dt.Columns.Add("货位名称",typeof(string));
             dt.Columns.Add("入库位置", typeof(string));
             dt.Columns.Add("出库位置", typeof(string));
             foreach (var item in cellPosition)
@@ -181,6 +159,7 @@ namespace THOK.Wms.Bll.Service
                     (
                         item.ID,
                         item.CellCode,
+                        item.CellName,
                         item.InName,
                         item.OutName
                     );
