28355d0289654c3871e492fffbea828ef08b01bc
loou <loou@vip.qq.com>
2013-07-05 18:07:09 +0800

    添加文本表 Signed-off-by: loou <loou@vip.qq.com>

---
+++ code/Authority/THOK.Wms.DbModel/Mapping/XmlValueMap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.Common.Ef.MappingStrategy;
+
+namespace THOK.Wms.DbModel.Mapping
+{
+   public class XmlValueMap : EntityMappingBase<XmlValue>
+    {
+       public XmlValueMap()
+            : base("Wms")
+        {
+            // Primary Key
+            this.HasKey(t => t.ID);
+
+            // Properties
+            this.Property(t => t.DateTime);               
+
+            this.Property(t => t.XmlValue)
+                .IsMaxLength();
+
+            // Table & Column Mappings
+            this.Property(t => t.ID).HasColumnName(ColumnMap.Value.To("ID"));
+            this.Property(t => t.DateTime).HasColumnName(ColumnMap.Value.To("DateTime"));
+            this.Property(t => t.XmlValue).HasColumnName(ColumnMap.Value.To("XmlValue"));
+            
+            // Relationships
+        }
+    }
+}
---
+++ code/Authority/THOK.Wms.DbModel/XmlValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace THOK.Wms.DbModel
+{
+   public class XmlValue
+    {
+        public Guid ID { get; set; }
+        public DateTime DateTime { get; set; }
+        public string XmlValue { get; set; }
+    }
+}
