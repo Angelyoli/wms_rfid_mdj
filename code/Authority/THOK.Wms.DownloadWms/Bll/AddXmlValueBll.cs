a1112af7ecb87473cc181efbb5b4569e31992432
loou <loou@vip.qq.com>
2013-07-22 12:40:36 +0800

    添加led大屏程序 Signed-off-by: loou <loou@vip.qq.com>

--- code/Authority/THOK.Wms.DownloadWms/Bll/AddXmlValueBll.cs
+++ code/Authority/THOK.Wms.DownloadWms/Bll/AddXmlValueBll.cs
@@ -13,7 +13,7 @@ namespace THOK.Wms.DownloadWms.Bll
        {
            using (PersistentManager dbpm = new PersistentManager())
            {
-               string sql = @"DELETE WMS_XML_VALUE WHERE [DATETIME]<DATEADD(DAY, -3, CONVERT(VARCHAR(14), GETDATE(), 112)) 
+               string sql = @"DELETE WMS_XML_VALUE WHERE [DATE_TIME]<DATEADD(DAY, -3, CONVERT(VARCHAR(14), GETDATE(), 112)) 
                               INSERT INTO WMS_XML_VALUE(ID,DATE_TIME,XML_VALUE_TEXT)VALUES('" + Guid.NewGuid() + "_" + id + "','" + DateTime.Now + "','" + xml + "')";
                AddXmlValueDao dao = new AddXmlValueDao();
                dao.Insert(sql);
