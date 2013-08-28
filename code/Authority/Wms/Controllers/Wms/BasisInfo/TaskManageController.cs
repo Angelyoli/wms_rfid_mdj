b46b05856ffc388a28fbe2c3256bd0c8c811bc75
git1207 <git1302@163.com>
2013-08-28 12:37:28 +0800

    【任务管理】创建任务管理模块。

---
+++ code/Authority/Wms/Controllers/Wms/BasisInfo/TaskManageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wms.Controllers.WMS.BasisInfo
+{
+    public class TaskManageController : Controller
+    {
+        //
+        // GET: /TaskManage/
+
+        public ActionResult Index(string moduleID)
+        {
+            ViewBag.hasSearch = true;
+            ViewBag.hasAdd = true;
+            ViewBag.hasEdit = true;
+            ViewBag.hasDelete = true;
+            ViewBag.hasPrint = true;
+            ViewBag.hasHelp = true;
+            ViewBag.ModuleID = moduleID;
+            return View();
+        }
+
+    }
+}
