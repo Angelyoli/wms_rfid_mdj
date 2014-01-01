19c9aad844734b3b7af9c4d9af214f5c835c525a
git1207 <git1302@163.com>
2014-01-01 11:44:36 +0800

    盘点打印功能按照货位排序

--- code/Authority/THOK.Wms.Bll/Service/CheckBillDetailService.cs
+++ code/Authority/THOK.Wms.Bll/Service/CheckBillDetailService.cs
@@ -136,7 +136,7 @@ namespace THOK.Wms.Bll.Service
 
         #endregion
 
-        #region ICheckBillDetailService 成员
+        #region 打印
         /// <summary>获得盘点细单信息</summary>
         public System.Data.DataTable GetCheckBillDetail(int page, int rows, string BillNo)
         {
@@ -144,7 +144,7 @@ namespace THOK.Wms.Bll.Service
             IQueryable<CheckBillDetail> checkBillDetailQuery = CheckBillDetailRepository.GetQueryable();
             if (BillNo != null && BillNo != string.Empty)
             {
-                var checkBillDetail = checkBillDetailQuery.Where(i => i.BillNo.Contains(BillNo)).OrderBy(i => i.BillNo).Select(i => new
+                var checkBillDetail = checkBillDetailQuery.Where(i => i.BillNo.Contains(BillNo)).OrderBy(i => i.CellCode).Select(i => new
                 {
                     i.BillNo,
                     i.Cell.CellCode,
