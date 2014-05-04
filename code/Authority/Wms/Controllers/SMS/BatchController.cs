968d3b8f20e65d480cb71862c53999a6d644c135
zengwh <545510993@qq.com>
2014-05-04 14:32:27 +0800

    完善批次号增删改查功能

--- code/Authority/Wms/Controllers/SMS/BatchController.cs
+++ code/Authority/Wms/Controllers/SMS/BatchController.cs
@@ -28,6 +28,8 @@ namespace Wms.Controllers.SMS
             ViewBag.hasAdd = true;
             ViewBag.hasEdit = true;
             ViewBag.hasDelete = true;
+            //ViewBag.hasAudit = true;
+            //ViewBag.hasAntiTrial = true;
             ViewBag.ModuleID = moduleID;
             return View();
         }
@@ -44,37 +46,40 @@ namespace Wms.Controllers.SMS
 
         public ActionResult Details(int page, int rows, FormCollection collection)
         {
-            Batch batchInfo = new Batch();
-            batchInfo.BatchId = Convert.ToInt32(collection["BatchID"]);
-            batchInfo.BatchName = collection["BatchName"] ?? "";
-            batchInfo.BatchNo = Convert.ToInt32(collection["BatchNo"]);
-            batchInfo.Description = collection["Description"] ?? "";
-            var srmDetail = BatchInfoService.GetDetails(page, rows, batchInfo);
+        
+            string BatchName = collection["BatchName"] ?? "";     
+            string OrderDate = collection["OrderDate"] ?? "";
+            string OperateDate = collection["OperateDate"] ?? "";
+            string BatchNo = collection["BatchNo"] ?? "";
+
+            var srmDetail = BatchInfoService.GetDetails(page, rows, BatchNo, BatchName, OrderDate, OperateDate);
             return Json(srmDetail, "text", JsonRequestBehavior.AllowGet);
+               
+           
         }
 
         public ActionResult Create(Batch batchInfo)
         {
             string strResult = string.Empty;
-            bool bResult = BatchInfoService.Add(batchInfo, out strResult);
+            bool bResult = BatchInfoService.Add(batchInfo, this.User.Identity.Name.ToString(),out strResult);
             string msg = bResult ? "新增成功" : "新增失败";
             return Json(JsonMessageHelper.getJsonMessage(bResult, msg, strResult), "text", JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Edit(Batch batchInfoCode)
+        public ActionResult Edit(Batch batchInfo)
         {
             string strResult = string.Empty;
-            bool bResult = BatchInfoService.Save(batchInfoCode, out strResult);
+            bool bResult = BatchInfoService.Save(batchInfo, out strResult);
             string msg = bResult ? "修改成功" : "修改失败";
             return Json(JsonMessageHelper.getJsonMessage(bResult, msg, strResult), "text", JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Delete(string batchcode)
+        public ActionResult Delete(int batchId)
         {
             string strResult = string.Empty;
-            bool bResult = BatchInfoService.Delete(batchcode, out strResult);
+            bool bResult = BatchInfoService.Delete(batchId, out strResult);
             string msg = bResult ? "删除成功" : "删除失败";
             return Json(JsonMessageHelper.getJsonMessage(bResult, msg, strResult), "text", JsonRequestBehavior.AllowGet);
-        }     
+        }
     }
 }
