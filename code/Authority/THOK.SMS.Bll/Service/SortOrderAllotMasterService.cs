a36cb81e06f1c3d3597f66f01ea641d942adf9b5
zqb <940658539@qq.com>
2014-05-15 16:39:14 +0800

    修改 sms系统 实现分拣订单信息页面

--- code/Authority/THOK.SMS.Bll/Service/SortOrderAllotMasterService.cs
+++ code/Authority/THOK.SMS.Bll/Service/SortOrderAllotMasterService.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using THOK.Wms.SignalR.Common;
 using Microsoft.Practices.Unity;
 using THOK.Common.Entity;
+using System.Data;
 
 namespace THOK.SMS.Bll.Service
 {
@@ -14,18 +15,130 @@ namespace THOK.SMS.Bll.Service
         [Dependency]
         public ISortOrderAllotMasterRepository SortOrderAllotMasterRepository { get; set; }
 
+        [Dependency]
+        public ISortOrderAllotDetailService SortOrderAllotDetailService { get; set; }
+
         protected override Type LogPrefix
         {
             get { return this.GetType(); }
         }
 
 
-        public object GetDetails(int page, int rows, SortOrderAllotMaster SortOrderAllotMaster)
+        public object GetDetails(int page, int rows, string batchNo, string orderId, string status)
         {
-            //
-            //   未实现。。
-            //
-            return SortOrderAllotMaster;
+            IQueryable<SortOrderAllotMaster> sortOrderAllotMasterQuery = SortOrderAllotMasterRepository.GetQueryable();
+            var sortOrderAllotMasterDetails = sortOrderAllotMasterQuery.Where(s => s.OrderId.Contains(orderId)&&s.Status.Contains(status));
+            if (batchNo != "")
+            {
+                int no;
+                Int32.TryParse(batchNo,out no);
+                if (no != 0)
+                {
+                    sortOrderAllotMasterDetails = sortOrderAllotMasterDetails.Where(s => s.batchSort.batch.BatchNo == no);
+                }
+            }
+            int total = sortOrderAllotMasterDetails.Count();
+            var sortOrderAllotMasterArray = sortOrderAllotMasterDetails.OrderBy(s => s.PackNo).Skip((page - 1) * rows).Take(rows)
+                .Select(s => new
+                {
+                    s.OrderMasterCode,
+                    s.BatchSortId,
+                    OrderDate = s.batchSort.batch.OrderDate,
+                    BatchNo=s.batchSort.batch.BatchNo,
+                    s.PackNo,
+                    s.OrderId,
+                    s.CustomerOrder,
+                    s.CustomerDeliverOrder,
+                    s.Quantity,
+                    s.ExportNo,
+                    StartTime=s.StartTime,
+                    FinishTime = s.FinishTime,
+                    Status = s.Status == "01" ? "已分配" : s.Status == "02" ? "已中止" : s.Status == "03" ? "已完成" : "已结单"
+                }).ToArray();
+            var orderMaster = sortOrderAllotMasterArray.Select(o => new
+            {
+                o.OrderMasterCode,
+                o.BatchSortId,
+                OrderDate = o.OrderDate.ToString("yyyy-MM-dd"),
+                BatchNo = o.BatchNo,
+                o.PackNo,
+                o.OrderId,
+                o.CustomerOrder,
+                o.CustomerDeliverOrder,
+                o.Quantity,
+                o.ExportNo,
+                StartTime = o.StartTime.ToString("yyyy-MM-dd"),
+                FinishTime = o.FinishTime.ToString("yyyy-MM-dd"),
+                o.Status
+            });
+            return new { total, rows = orderMaster .ToArray()};
+        }
+
+        public DataTable GetSortOrderAllotMaster(int page,int rows,string batchNo, string orderId, string status)
+        {
+            IQueryable<SortOrderAllotMaster> sortOrderAllotMasterQuery = SortOrderAllotMasterRepository.GetQueryable();
+            var sortOrderAllotMasterDetails = sortOrderAllotMasterQuery.Where(s => s.OrderId.Contains(orderId) && s.Status.Contains(status));
+            if (batchNo != "")
+            {
+                int no;
+                Int32.TryParse(batchNo, out no);
+                if (no != 0)
+                {
+                    sortOrderAllotMasterDetails = sortOrderAllotMasterDetails.Where(s => s.batchSort.batch.BatchNo == no);
+                }
+            }
+            int total = sortOrderAllotMasterDetails.Count();
+            var sortOrderAllotMasterArray = sortOrderAllotMasterDetails.OrderBy(s => s.PackNo).Skip((page - 1) * rows).Take(rows)
+                .Select(s => new
+                {
+                    s.OrderMasterCode,
+                    s.BatchSortId,
+                    OrderDate = s.batchSort.batch.OrderDate,
+                    BatchNo = s.batchSort.batch.BatchNo,
+                    s.PackNo,
+                    s.OrderId,
+                    s.CustomerOrder,
+                    s.CustomerDeliverOrder,
+                    s.Quantity,
+                    s.ExportNo,
+                    StartTime = s.StartTime,
+                    FinishTime = s.FinishTime,
+                    Status = s.Status == "01" ? "已分配" : s.Status == "02" ? "已中止" : s.Status == "03" ? "已完成" : "已结单"
+                }).ToArray();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("主单代码", typeof(string));
+            dt.Columns.Add("订单日期", typeof(string));
+            dt.Columns.Add("批次号", typeof(string));
+            dt.Columns.Add("烟包包号", typeof(string));
+            dt.Columns.Add("订单编码", typeof(string));
+            dt.Columns.Add("客户顺序", typeof(string));
+            dt.Columns.Add("配送顺序", typeof(string));
+            dt.Columns.Add("烟包数量", typeof(string));
+            dt.Columns.Add("开始时间", typeof(string));
+            dt.Columns.Add("完成时间", typeof(string));
+            dt.Columns.Add("任务状态", typeof(string));
+            foreach (var item in sortOrderAllotMasterArray)
+            {
+                dt.Rows.Add(
+                    item.OrderMasterCode,
+                    item.OrderDate.ToString("yyyy-MM-dd"),
+                    item.BatchNo,
+                    item.PackNo,
+                    item.OrderId,
+                    item.CustomerOrder,
+                    item.CustomerDeliverOrder,
+                    item.Quantity,
+                    item.StartTime.ToString("yyyy-MM-dd"),
+                    item.FinishTime.ToString("yyyy-MM-dd"),
+                    item.Status);
+                dt.Rows.Add("","","","细单代码","主单代码","商品编码","商品名称","烟道代码","数量","","");
+                var sortOrderAllotDetail = SortOrderAllotDetailService.GetDetailsByOrderMasterCode(item.OrderMasterCode);
+                foreach (DataRow ss in sortOrderAllotDetail.Rows)
+                {
+                    dt.ImportRow(ss);
+                }
+            }
+            return dt;
         }
     }
 }
---
+++ code/Authority/Wms/Controllers/SMS/SortOrderAllotController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using THOK.SMS.DbModel;
+using Microsoft.Practices.Unity;
+using THOK.SMS.Bll.Interfaces;
+using THOK.Common.NPOI.Models;
+using THOK.Common.NPOI.Service;
+
+namespace Wms.Controllers.SMS
+{
+    public class SortOrderAllotController : Controller
+    {
+        [Dependency]
+        public ISortOrderAllotMasterService SortOrderAllotMaster { get; set; }
+
+        [Dependency]
+        public ISortOrderAllotDetailService SortOrderAllotDetailService { get; set; }
+
+        //
+        // GET: /SortOrderAllot/
+
+        public ActionResult Index(string moduleID)
+        {
+            ViewBag.hasSearch = true;
+            ViewBag.hasPrint = true;
+            ViewBag.hasHelp = true;
+            ViewBag.ModuleID = moduleID;
+            return View();
+        }
+
+        public ActionResult OrderMaster(int page, int rows, string batchNo, string orderId, string status)
+        {
+            batchNo = batchNo ?? "";
+            orderId = orderId ?? "";
+            status = status ?? "";
+            var sortOrderMaster = SortOrderAllotMaster.GetDetails(page, rows, batchNo, orderId, status);
+            return Json(sortOrderMaster, "text", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult OrderDetails(int page, int rows, string orderMasterCode)
+        {
+            var sortOrderDetail = SortOrderAllotDetailService.GetDetails(page, rows, orderMasterCode);
+            return Json(sortOrderDetail, "text", JsonRequestBehavior.AllowGet);
+        }
+
+        //打印
+        public FileStreamResult CreateExcelToClient()
+        {
+            int page;
+            Int32.TryParse(Request.QueryString["page"], out page);
+            int rows;
+            Int32.TryParse(Request.QueryString["rows"], out rows);
+            string batchNo = Request.QueryString["batchNo"] ?? "";
+            string orderId = Request.QueryString["orderId"] ?? "";
+            string status = Request.QueryString["status"] ?? "";
+
+            ExportParam ep = new ExportParam();
+            ep.DT1 = SortOrderAllotMaster.GetSortOrderAllotMaster(page,rows,batchNo, orderId, status);
+            ep.HeadTitle1 = "烟道信息";
+            return PrintService.Print(ep);
+        }
+    }
+}
