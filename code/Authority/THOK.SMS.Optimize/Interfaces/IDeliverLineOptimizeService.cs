c67c8fa4a478e329af8b70ab1e54e87f571318c8
Endis <zhengzhibin@skyseaok.com>
2014-05-19 10:17:08 +0800

    SMS。Optimize..Init

---
+++ code/Authority/THOK.SMS.Optimize/Interfaces/IDeliverLineOptimizeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.SMS.DbModel;
+
+namespace THOK.SMS.Optimize.Interfaces
+{
+     public interface IDeliverLineOptimizeService
+    {
+         bool OptimizeAllot(int batchNo, out string strResult);
+    }
+}
---
+++ code/Authority/THOK.SMS.Optimize/Service/DeliverLineOptimizeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using THOK.SMS.DbModel;
+using THOK.SMS.Dal.Interfaces;
+using THOK.SMS.Optimize.Interfaces;
+using System.Linq;
+using THOK.Common.Entity;
+
+namespace THOK.SMS.Optimize.Service
+{
+    public class DeliverLineOptimizeService
+    {
+        public bool OptimizeAllot(int batchNo, out string strResult)
+        {
+            strResult = "";
+            return true;
+        }
+    }
+}
